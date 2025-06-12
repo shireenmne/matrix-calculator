a6eb5f35c0f756c81013bf96afe46f600e9cd162
shireenmne <samennane@gmail.com>
2025-06-12 16:07:53 +0100

    Update matrix.py

--- operations/matrix.py
+++ operations/matrix.py
@@ -7,4 +7,13 @@ class Matrix:
         if any(len(row) != self.cols for row in data):
             raise ValueError("All rows must have the same number of columns")
         
-        
+    def __str__(self):
+        row_strings=[]
+        for row in self.data:
+            str_elements = []
+            for element in row:
+                str_elements.append(str(element))
+            row_strings = '\t'.join(str_elements)
+            row_strings.append(row_strings)
+            matrix_string = '\n'.join(row_strings)
+        return matrix_string
